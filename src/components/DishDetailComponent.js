05b55990ba6c74e22036fc5abe0f3a226f69a826
TheRoro <39977138+TheRoro@users.noreply.github.com>
2020-08-08 13:53:39 -0500

    Redux Thunk

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -3,6 +3,7 @@ import { Card, CardImg,  CardText, CardBody, CardTitle, Button, Modal,
 ModalHeader, ModalBody, Label, Col, Row, Breadcrumb, BreadcrumbItem  } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -111,7 +112,7 @@ class CommentForm extends Component {
             return(
                 <div className="col-12 col-md-5 m-1">
                     <h4>Comments:</h4>
-                    <ul class="list-unstyled">
+                    <ul className="list-unstyled">
                         {comments.map((comment) => {
                             return(
                                 <li key={comment.id}>
@@ -133,7 +134,25 @@ class CommentForm extends Component {
     }
 
     const DishDetail = (props) => {
-        if(props.dish != null){
+        if(props.isLoading){
+            return(
+                <div className="container">
+                    <div className="row">
+                        <Loading/>
+                    </div>
+                </div>
+            );
+        }
+        else if(props.errMess) {
+            return(
+                <div className="container">
+                    <div className="row">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            )
+        }
+        else if(props.dish != null){
             return (
                 <div className="container">
                     <div className="row">
